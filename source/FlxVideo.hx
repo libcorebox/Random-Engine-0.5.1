336cd862dc4eb9e72d1da4eceac0d91bffdb7c08
L <you@example.com>
2022-05-14 15:38:05 -0400

    vlc

--- source/FlxVideo.hx
+++ source/FlxVideo.hx
@@ -3,27 +3,21 @@ import openfl.net.NetConnection;
 import openfl.net.NetStream;
 import openfl.events.NetStatusEvent;
 import openfl.media.Video;
-#elseif android
-import extension.webview.WebView;
-import android.AndroidTools;
-#else
-import openfl.events.Event;
-import vlc.VlcBitmap;
+#elseif !web
+import vlc.MP4Handler;
 #end
 import flixel.FlxBasic;
 import flixel.FlxG;
 
-class FlxVideo extends FlxBasic
-{
+class FlxVideo extends FlxBasic {
 	#if VIDEOS_ALLOWED
 	public var finishCallback:Void->Void = null;
-
-	#if desktop
-	public static var vlcBitmap:VlcBitmap;
+	
+	#if !web
+	public static var video:MP4Handler;
 	#end
 
-	public function new(name:String)
-	{
+	public function new(name:String) {
 		super();
 
 		#if web
@@ -35,122 +29,34 @@ class FlxVideo extends FlxBasic
 		netConnect.connect(null);
 		var netStream = new NetStream(netConnect);
 		netStream.client = {
-			onMetaData: function()
-			{
+			onMetaData: function() {
 				player.attachNetStream(netStream);
 				player.width = FlxG.width;
 				player.height = FlxG.height;
 			}
 		};
-		netConnect.addEventListener(NetStatusEvent.NET_STATUS, function(event:NetStatusEvent)
-		{
-			if (event.info.code == "NetStream.Play.Complete")
-			{
+		netConnect.addEventListener(NetStatusEvent.NET_STATUS, function(event:NetStatusEvent) {
+			if(event.info.code == "NetStream.Play.Complete") {
 				netStream.dispose();
-				if (FlxG.game.contains(player))
-					FlxG.game.removeChild(player);
+				if(FlxG.game.contains(player)) FlxG.game.removeChild(player);
 
-				if (finishCallback != null)
-					finishCallback();
+				if(finishCallback != null) finishCallback();
 			}
 		});
 		netStream.play(name);
-		#elseif android
-		WebView.playVideo(AndroidTools.getFileUrl(name), true);
-		WebView.onComplete = function()
+
+		#elseif !web
+		// by Polybius, check out hxCodec! https://github.com/polybiusproxy/hxCodec
+
+		video = new MP4Handler();
+		video.finishCallback = function()
 		{
-			if (finishCallback != null)
-			{
-				finishCallback();
-			}
+				if (finishCallback != null){
+			        finishCallback();
+		        }
 		}
-		#elseif desktop
-		// by Polybius, check out PolyEngine! https://github.com/polybiusproxy/PolyEngine
-
-		vlcBitmap = new VlcBitmap();
-		vlcBitmap.set_height(FlxG.stage.stageHeight);
-		vlcBitmap.set_width(FlxG.stage.stageHeight * (16 / 9));
-
-		vlcBitmap.onComplete = onVLCComplete;
-		vlcBitmap.onError = onVLCError;
-
-		FlxG.stage.addEventListener(Event.ENTER_FRAME, fixVolume);
-		vlcBitmap.repeat = 0;
-		vlcBitmap.inWindow = false;
-		vlcBitmap.fullscreen = false;
-		fixVolume(null);
-
-		FlxG.addChildBelowMouse(vlcBitmap);
-		vlcBitmap.play(checkFile(name));
+		video.playVideo(name);
 		#end
 	}
-
-	#if desktop
-	function checkFile(fileName:String):String
-	{
-		var pDir = "";
-		var appDir = "file:///" + Sys.getCwd() + "/";
-
-		if (fileName.indexOf(":") == -1) // Not a path
-			pDir = appDir;
-		else if (fileName.indexOf("file://") == -1 || fileName.indexOf("http") == -1) // C:, D: etc? ..missing "file:///" ?
-			pDir = "file:///";
-
-		return pDir + fileName;
-	}
-
-	public static function onFocus()
-	{
-		if (vlcBitmap != null)
-		{
-			vlcBitmap.resume();
-		}
-	}
-
-	public static function onFocusLost()
-	{
-		if (vlcBitmap != null)
-		{
-			vlcBitmap.pause();
-		}
-	}
-
-	function fixVolume(e:Event)
-	{
-		// shitty volume fix
-		vlcBitmap.volume = 0;
-		if (!FlxG.sound.muted && FlxG.sound.volume > 0.01)
-		{ // Kind of fixes the volume being too low when you decrease it
-			vlcBitmap.volume = FlxG.sound.volume * 0.5 + 0.5;
-		}
-	}
-
-	public function onVLCComplete()
-	{
-		vlcBitmap.stop();
-
-		// Clean player, just in case!
-		vlcBitmap.dispose();
-
-		if (FlxG.game.contains(vlcBitmap))
-		{
-			FlxG.game.removeChild(vlcBitmap);
-		}
-
-		if (finishCallback != null)
-		{
-			finishCallback();
-		}
-	}
-
-	function onVLCError()
-	{
-		trace("An error has occured while trying to load the video.\nPlease, check if the file you're loading exists.");
-		if (finishCallback != null)
-		{
-			finishCallback();
-		}
-	}
-	#end
 	#end
 }
--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1590,64 +1590,50 @@ class PlayState extends MusicBeatState
 		char.y += char.positionArray[1];
 	}
 
-	public function startVideo(name:String):Void
-	{
-	#if VIDEOS_ALLOWED
-	var foundFile:Bool = false;
-	var fileName:String = #if MODS_ALLOWED Paths.modFolders('videos/' + name + '.' + Paths.VIDEO_EXT); #else ''; #end
-	#if sys
-	if (FileSystem.exists(fileName))
-	{
-		foundFile = true;
-	}
-	#end
-
-	if (!foundFile)
-	{
-		fileName = Paths.video(name);
+	public function startVideo(name:String):Void {
+		#if VIDEOS_ALLOWED
+		var foundFile:Bool = false;
+		var fileName:String = #if MODS_ALLOWED Paths.modFolders('videos/' + name + '.' + Paths.VIDEO_EXT); #else ''; #end
 		#if sys
-		if (FileSystem.exists(fileName))
-		{
-		#else
-		if (OpenFlAssets.exists(fileName))
-		{
-		#end
+		if(FileSystem.exists(fileName)) {
 			foundFile = true;
 		}
-		} if (foundFile)
+		#end
 
-		{
+		if(!foundFile) {
+			fileName = Paths.video(name);
+			#if sys
+			if(FileSystem.exists(fileName)) {
+			#else
+			if(OpenFlAssets.exists(fileName)) {
+			#end
+				foundFile = true;
+			}
+		}
+
+		if(foundFile) {
 			inCutscene = true;
 			var bg = new FlxSprite(-FlxG.width, -FlxG.height).makeGraphic(FlxG.width * 3, FlxG.height * 3, FlxColor.BLACK);
 			bg.scrollFactor.set();
 			bg.cameras = [camHUD];
 			add(bg);
 
-			(new FlxVideo(fileName)).finishCallback = function()
-			{
+			(new FlxVideo(fileName)).finishCallback = function() {
 				remove(bg);
-				if (endingSong)
-				{
+				if(endingSong) {
 					endSong();
-				}
-				else
-				{
+				} else {
 					startCountdown();
 				}
 			}
 			return;
-		}
-		else
-		{
+		} else {
 			FlxG.log.warn('Couldnt find video file: ' + fileName);
 		}
 		#end
-		if (endingSong)
-		{
+		if(endingSong) {
 			endSong();
-		}
-		else
-		{
+		} else {
 			startCountdown();
 		}
 	}
